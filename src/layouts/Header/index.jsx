efefd30b945e35e0b782e041832c2dff1e088d50
Néphélim <nephcohen@gmail.com>
2024-09-24 08:35:40 +0200

    Version beta

--- src/layouts/Header/index.jsx
+++ src/layouts/Header/index.jsx
@@ -1,10 +1,10 @@
 import scssHeader from './Header.module.scss';
 
 const Header = () => {
-    return (
-        <header className={scssHeader.header}>
-            <p className={scssHeader.beta}>Beta early access only - v 0.2.1</p>
-        </header>
-    );
-}
+  return (
+    <header className={scssHeader.header}>
+      <p className={scssHeader.beta}>Beta early access only - v 0.2.4</p>
+    </header>
+  );
+};
 export default Header;
